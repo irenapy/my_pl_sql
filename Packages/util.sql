f0a89f59484c37328e97dbb06ac01993339ff773
Ira Somyk <165530303+irenapy@users.noreply.github.com>
2025-02-07 10:13:19 +0100

    Update util.sql

--- Packages/util.sql
+++ Packages/util.sql
@@ -1,4 +1,17 @@
 ---------------------------------------------------------------SPECIFICATION-------------------------------------------------------------------------------
+PROCEDURE change_attribute_employee(
+    p_employee_id      IN NUMBER, 
+    p_first_name       IN VARCHAR2 DEFAULT NULL,
+    p_last_name        IN VARCHAR2 DEFAULT NULL,
+    p_email            IN VARCHAR2 DEFAULT NULL,
+    p_phone_number     IN VARCHAR2 DEFAULT NULL,
+    p_job_id           IN VARCHAR2 DEFAULT NULL,
+    p_salary           IN NUMBER DEFAULT NULL,
+    p_commission_pct   IN NUMBER DEFAULT NULL,
+    p_manager_id       IN NUMBER DEFAULT NULL,
+    p_department_id    IN NUMBER DEFAULT NULL
+);
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
 PROCEDURE is_business_hours;
 -------------------------------------------------------------------------------------------------------------------------------------------------------------
 PROCEDURE fire_an_employee (p_employee_id IN NUMBER);   
@@ -15,6 +28,93 @@ PROCEDURE add_employee (
         p_manager_id     IN NUMBER DEFAULT 100,
         p_department_id  IN NUMBER);
 ---------------------------------------------------------------BODY------------------------------------------------------------------------------------------
+PROCEDURE change_attribute_employee(
+    p_employee_id      IN NUMBER, 
+    p_first_name       IN VARCHAR2 DEFAULT NULL,
+    p_last_name        IN VARCHAR2 DEFAULT NULL,
+    p_email            IN VARCHAR2 DEFAULT NULL,
+    p_phone_number     IN VARCHAR2 DEFAULT NULL,
+    p_job_id           IN VARCHAR2 DEFAULT NULL,
+    p_salary           IN NUMBER DEFAULT NULL,
+    p_commission_pct   IN NUMBER DEFAULT NULL,
+    p_manager_id       IN NUMBER DEFAULT NULL,
+    p_department_id    IN NUMBER DEFAULT NULL
+) IS
+    v_employee_ex NUMBER;
+BEGIN
+    
+    log_util.log_start('change_attribute_employee');
+
+     SELECT COUNT(*) INTO v_employee_ex FROM employees WHERE employee_id = p_employee_id;
+    
+    IF v_employee_ex = 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Переданий співробітник не існує');
+    END IF;
+
+  
+    IF p_first_name IS NULL 
+       AND p_last_name IS NULL 
+       AND p_email IS NULL 
+       AND p_phone_number IS NULL 
+       AND p_job_id IS NULL 
+       AND p_salary IS NULL 
+       AND p_commission_pct IS NULL 
+       AND p_manager_id IS NULL 
+       AND p_department_id IS NULL THEN
+        log_util.log_finish('change_attribute_employee');
+        RAISE_APPLICATION_ERROR(-20001, 'Помилка: Значення NULL');
+    END IF;
+ 
+    BEGIN
+  
+        IF p_first_name IS NOT NULL THEN
+            UPDATE employees SET first_name = p_first_name WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_last_name IS NOT NULL THEN
+            UPDATE employees SET last_name = p_last_name WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_email IS NOT NULL THEN
+            UPDATE employees SET email = p_email WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_phone_number IS NOT NULL THEN
+            UPDATE employees SET phone_number = p_phone_number WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_job_id IS NOT NULL THEN
+            UPDATE employees SET job_id = p_job_id WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_salary IS NOT NULL THEN
+            UPDATE employees SET salary = p_salary WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_commission_pct IS NOT NULL THEN
+            UPDATE employees SET commission_pct = p_commission_pct WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_manager_id IS NOT NULL THEN
+            UPDATE employees SET manager_id = p_manager_id WHERE employee_id = p_employee_id;
+        END IF;
+
+        IF p_department_id IS NOT NULL THEN
+            UPDATE employees SET department_id = p_department_id WHERE employee_id = p_employee_id;
+        END IF;
+
+        COMMIT;
+       
+        DBMS_OUTPUT.PUT_LINE('Дані співробітника оновлені.');
+    EXCEPTION
+        WHEN OTHERS THEN
+            log_util.log_error('change_attribute_employee', SQLERRM);
+            RAISE;
+    END;
+
+    log_util.log_finish('change_attribute_employee');
+END change_attribute_employee;
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
 PROCEDURE is_business_hours IS
 BEGIN
     IF TO_CHAR(SYSDATE, 'DY', 'NLS_DATE_LANGUAGE=AMERICAN') IN ('SAT', 'SUN') THEN
