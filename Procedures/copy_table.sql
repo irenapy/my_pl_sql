db3c6378a1f685f6c83889441287b3faddf0fa14
Ira Somyk <165530303+irenapy@users.noreply.github.com>
2025-02-11 15:02:09 +0100

    Add files via upload

---
+++ Procedures/copy_table.sql
@@ -0,0 +1,52 @@
+CREATE OR REPLACE PROCEDURE copy_table(
+    p_source_scheme IN VARCHAR2,
+    p_target_scheme IN VARCHAR2 DEFAULT USER,
+    p_copy_data IN BOOLEAN DEFAULT FALSE,
+    po_result OUT VARCHAR2
+) AS
+    v_table_name VARCHAR2(255);
+    v_code VARCHAR2(4000);
+    v_sql VARCHAR2(4000);
+BEGIN
+    FOR r IN (
+        SELECT table_name, 
+               'CREATE TABLE ' || p_target_scheme || '.' || table_name || ' (' ||
+               LISTAGG(column_name || ' ' || data_type || count_symbol, ', ') WITHIN GROUP (ORDER BY column_id) || ')' AS ddl_code
+        FROM (
+            SELECT table_name, column_name, data_type,
+                   CASE
+                       WHEN data_type IN ('VARCHAR2', 'CHAR') THEN '(' || data_length || ')'
+                       WHEN data_type = 'DATE' THEN NULL
+                       WHEN data_type = 'NUMBER' THEN REPLACE('(' || data_precision || ',' || data_scale || ')', '(,)', NULL)
+                   END AS count_symbol,
+                   column_id
+            FROM all_tab_columns
+            WHERE owner = 'IRINA_II6'
+              AND table_name IN ('EMPLOYEES', 'LOGS')  
+        )
+        GROUP BY table_name
+    ) LOOP
+        BEGIN
+            v_table_name := r.table_name;
+            v_code := r.ddl_code;
+            EXECUTE IMMEDIATE v_code;
+            to_log('Таблиця ' || v_table_name || ' створена у ' || p_target_scheme, 'INFO');
+
+            IF p_copy_data THEN
+                v_sql := 'INSERT INTO ' || p_target_scheme || '.' || v_table_name || 
+                         ' SELECT * FROM ' || p_source_scheme || '.' || v_table_name;
+                EXECUTE IMMEDIATE v_sql;
+                to_log('Дані скопійовано ' || v_table_name, 'INFO');
+            END IF;
+        EXCEPTION
+            WHEN OTHERS THEN
+                to_log('Помилка при створенні ' || v_table_name || ': ' || SQLERRM, 'ERROR');
+                CONTINUE;
+        END;
+    END LOOP;
+    po_result := 'Таблиця скопійована';
+EXCEPTION
+    WHEN OTHERS THEN
+        po_result := 'Error: ' || SQLERRM;
+        to_log(po_result, 'ПОМИЛКА');
+END copy_table;
